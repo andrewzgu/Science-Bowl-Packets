0e62e987361da27d84a82d1ae870d047b30bb525
Andrew Gu <agu1@mit.edu>
2020-10-28 14:16:00 -0400

    Further additions to compile script
    The script now reads a NUMBER_OF_ROUNDS variable from a file named
    config.cfg and warn the user when it is missing.

--- compile.sh
+++ compile.sh
@@ -1,27 +1,46 @@
 #!/usr/bin/env bash
 
+if [[ ! -f config.cfg ]] ; then
+  echo 'File "config.cfg" is missing. Making blank config file. Please add in the missing values.'
+  echo 'NUMBER_OF_ROUNDS=' > config.cfg
+  exit
+fi
+
+IFS="="
+while read -r name value
+do
+  export "$name"="$value"
+done < config.cfg
+
+if [[ -z "$NUMBER_OF_ROUNDS" ]]; then
+  echo 'Config file is missing NUMBER_OF_ROUNDS, aborting.'
+  exit
+fi
+
 while getopts "d" opt; do
   case $opt in
     d)
       draft=true
       ;;
+    *)
+      echo "Unrecognized flag"
+      ;;
   esac
 done
-
-draftstr="\\def\\draftmode{1}"
 
 rm -rf compiled
 mkdir -p compiled
 
-for i in {1..13}
+echo "Making $NUMBER_OF_ROUNDS rounds. Please check config.cfg if this is incorrect."
+
+for (( i=1; i<=NUMBER_OF_ROUNDS; i++ ))
 do
   filecontents="\\newcommand{\\roundnumber}{$i} \\input{roundtemplate.tex}"
   if [ "$draft" = true ]; then
-    filecontents="${draftstr} ${filecontents}"
+    filecontents="\\def\\draftmode{1} ${filecontents}"
   fi
-  echo "${filecontents}"
   pdflatex -output-directory compiled \
-    -interaction=nonstopmode \
+    -interaction=batchmode \
     -jobname="round$i" \
-    $filecontents
+    "$filecontents"
 done
