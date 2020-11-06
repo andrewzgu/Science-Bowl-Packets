0d5059146e80253db12622a81d6f06284214dbd9
Andrew Gu <agu1@mit.edu>
2020-11-05 23:16:28 -0500

    Adding support for tiebreaker rounds

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 if [[ ! -f config.cfg ]] ; then
   echo 'File "config.cfg" is missing. Making blank config file. Please add in the missing values.'
   echo 'NUMBER_OF_ROUNDS=' > config.cfg
+  echo 'NUMBER_OF_TB_ROUNDS=0' >> config.cfg
   exit
 fi
 
@@ -14,6 +15,11 @@ done < config.cfg
 
 if [[ -z "$NUMBER_OF_ROUNDS" ]]; then
   echo 'Config file is missing NUMBER_OF_ROUNDS, aborting.'
+  exit
+fi
+
+if [[ -z "$NUMBER_OF_TB_ROUNDS" ]]; then
+  echo 'Config file is missing NUMBER_OF_TB_ROUNDS, aborting.'
   exit
 fi
 
@@ -34,7 +40,7 @@ done
 rm -rf compiled
 mkdir -p compiled
 
-echo "Making $NUMBER_OF_ROUNDS rounds. Please check config.cfg if this is incorrect."
+echo "Making $NUMBER_OF_ROUNDS rounds."
 
 for (( i=1; i<=NUMBER_OF_ROUNDS; i++ ))
 do
@@ -49,5 +55,19 @@ do
   pdflatex -output-directory compiled \
     -interaction=batchmode \
     -jobname="round$i" \
+    "$filecontents"
+done
+
+echo "Making $NUMBER_OF_TB_ROUNDS tiebreaker rounds."
+
+for (( i=1; i<=NUMBER_OF_TB_ROUNDS; i++ ))
+do
+  filecontents="\\newcommand{\\roundnumber}{$i} \\input{tiebreaktemplate.tex}"
+  if [ "$draft" = true ]; then
+    filecontents="\\def\\draftmode{1} ${filecontents}"
+  fi
+  pdflatex -output-directory compiled \
+    -interaction=batchmode \
+    -jobname="tiebreakround$i" \
     "$filecontents"
 done
